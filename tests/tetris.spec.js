a2b11e9a926389ce23d7dded94848099df08f848
Igwe Kalu <igwejk@github.com>
2023-10-05 01:27:56 +0200

    Create reference continuous integration workflow

---
+++ tests/tetris.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+
+test("Tetris game", async ({ page }) => {
+  const SPACE_BAR = "";
+  const TETRIS_APP_PATH =
+    "pages/githubuniverseworkshops/github-devsecops-fundamentals";
+  const { TETRIS_APP_HOST, TETRIS_APP_PORT } = process.env;
+
+  await page.goto(
+    `http://${TETRIS_APP_HOST}:${TETRIS_APP_PORT}/${TETRIS_APP_PATH}`
+  );
+
+  await page.getByRole("link", { name: "Tetris" }).click();
+  await page.getByRole("link", { name: "play the game" }).click();
+
+  await expect(page.getByText("score 00000")).toBeVisible();
+  await expect(page.getByText("rows 0")).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Press Space to Play." })
+  ).toBeVisible();
+  await expect(page.locator("#upcoming")).toBeVisible();
+
+  await page.press(SPACE_BAR);
+});
