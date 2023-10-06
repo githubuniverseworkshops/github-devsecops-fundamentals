468de34494db60865d44c37b2f210ef05982c558
Igwe Kalu <igwejk@github.com>
2023-10-06 18:17:20 +0200

    Fix test

--- playwright.config.js
+++ playwright.config.js
@@ -11,6 +11,8 @@ const { defineConfig, devices } = require("@playwright/test");
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
+  timeout: 5 * 60 * 1000,
+  expect: { timeout: 1 * 60 * 1000 },
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
--- tests/tetris.spec.js
+++ tests/tetris.spec.js
@@ -4,19 +4,24 @@ test("Tetris game", async ({ page }) => {
   const SPACE_BAR = "";
   const { TETRIS_APP_HOST, TETRIS_APP_PORT, TETRIS_APP_PATH } = process.env;
 
+  // should be similar to http://127.0.0.1:8000/pages/githubuniverseworkshops/github-devsecops-fundamentals/
+  console.log(
+    `http://${TETRIS_APP_HOST}:${TETRIS_APP_PORT}/${TETRIS_APP_PATH}`
+  );
   await page.goto(
     `http://${TETRIS_APP_HOST}:${TETRIS_APP_PORT}/${TETRIS_APP_PATH}`
   );
 
-  await page.getByRole("link", { name: "Tetris" }).click();
-  await page.getByRole("link", { name: "play the game" }).click();
-
+  await page.getByRole("link", { name: "Tetris Game" }).click();
   await expect(page.getByText("score 00000")).toBeVisible();
   await expect(page.getByText("rows 0")).toBeVisible();
+  await expect(page.locator("#upcoming")).toBeVisible();
   await expect(
     page.getByRole("link", { name: "Press Space to Play." })
   ).toBeVisible();
-  await expect(page.locator("#upcoming")).toBeVisible();
 
-  await page.press(SPACE_BAR);
+  // await page.press(SPACE_BAR);
+  await page.getByRole("link", { name: "Press Space to Play." }).click();
+  await page.waitForTimeout(1 * 10 * 1000);
+  await expect(page.getByText("score 00000")).not.toBeVisible();
 });
